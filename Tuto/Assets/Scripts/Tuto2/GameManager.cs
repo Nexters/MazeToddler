f8e10eba029c4be10f4ef3bf1eb8cf135ae0d7b8
treehouse <treehouse@naver.com>
2015-07-18 15:15:41 +0900

    한솔이튜토리얼한거

---
+++ Tuto/Assets/Scripts/Tuto2/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject[] patterns;
+	public GameObject patternA;
+	public GameObject patternB;
+	public GameObject backCloudPrefab;
+	public GameObject frontCloudPrefab;
+	public GameObject backCloudA;
+	public GameObject backCloudB;
+	public GameObject frontCloud;
+
+	public float patternA_x = 0f;
+	public float patternB_x = 15f;
+	public float frontCloudSpawn_x = 16.5f;
+	public float frontCloudDestroy_x = -18f;
+
+	public float stepSpeed = 5f;
+	public float backCloudSpeed = 0.5f;
+	public float frontCloudSpeed = 10f;
+
+	void Update () {
+		if (patternA != null) {
+			patternA.transform.position += Vector3.left * Time.deltaTime * stepSpeed;
+		}
+		if (patternB != null) {
+			patternB.transform.position += Vector3.left * Time.deltaTime * stepSpeed;
+		}
+		if (patternB.transform.position.x <= patternA_x) {
+			if(patternA != null) Destroy (patternA);
+			patternA = patternB;
+			patternB = Instantiate (patterns[Random.Range(0,patterns.Length)], new Vector3(patternB_x,0,0), Quaternion.identity) as GameObject;
+		}
+
+		if (backCloudA != null) {
+			backCloudA.transform.position += Vector3.left * Time.deltaTime * backCloudSpeed;
+		}
+		if (backCloudB != null) {
+			backCloudB.transform.position += Vector3.left * Time.deltaTime * backCloudSpeed;
+		}
+		if (backCloudB.transform.position.x <= patternA_x) {
+			if(backCloudA != null) Destroy (backCloudA);
+			backCloudA = backCloudB;
+			backCloudB = Instantiate (backCloudPrefab, new Vector3(patternB_x,0,0), Quaternion.identity) as GameObject;
+		}
+
+		if (frontCloud != null) {
+			frontCloud.transform.position += Vector3.left * Time.deltaTime * frontCloudSpeed;
+		}
+		if (frontCloud.transform.position.x <= frontCloudDestroy_x) {
+			Destroy(frontCloud);
+			frontCloud = Instantiate (frontCloudPrefab, new Vector3(frontCloudSpawn_x, 0f, 0f), Quaternion.identity) as GameObject;
+		}
+	}
+}
---
+++ Tuto/Assets/Scripts/Tuto2/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 3f;
+
+	private Transform _transform;
+	private Rigidbody2D _rigidbody;
+
+	void Awake() {
+		_transform = transform;
+		_rigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if(Input.GetKey (KeyCode.LeftArrow)) {
+			_transform.position += Vector3.left * Time.deltaTime * speed;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			_transform.position += Vector3.right * Time.deltaTime * speed;
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			_rigidbody.AddForce(Vector2.up*10, ForceMode2D.Impulse);
+		}
+	}
+}
