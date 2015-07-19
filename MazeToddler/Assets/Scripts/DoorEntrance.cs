b5a0794e5627351b6bedb3c31d6303c5be942156
TaylorHan <rhaktkdl@gmail.com>
2015-07-19 21:37:55 +0900

    Proto Type Test Stage 제작 (Stage0)
    Stage0 제작 진행 (프로토타입 및 기술 테스트용)

---
+++ MazeToddler/Assets/Scripts/DoorEntrance.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorEntrance : MonoBehaviour {
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        print(string.Format("collider Tag = {0}", collider.transform.tag));
+        if (collider.transform.tag == "Player")
+        {
+            GameObject doorExit = GameObject.Find("Door_Exit");
+            Transform warpPos = doorExit.transform.FindChild("warpPos");
+            Vector3 exitPos = warpPos.position;
+            collider.transform.localPosition = exitPos;
+        }
+       
+    }
+}
---
+++ MazeToddler/Assets/Scripts/SceneStage0/SceneStageMgr0.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneStageMgr0 : MonoBehaviour {
+
+    //RuntimePlatform platform = Application.platform;
+
+    public void OnBtnResult()
+    {
+        Application.LoadLevel("SceneResult");
+    }
+
+    /*
+	public void OnQuitBtn()
+	{
+		Application.Quit();
+	}
+
+	void Update()
+	{
+		if( platform == RuntimePlatform.Android)
+		{
+			if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
+		}
+	}
+	*/
+}
